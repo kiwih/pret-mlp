04b62e44923c788dfe2e46625a106a657a95ea1c
kiwih <hammondpearce@outlook.com>
2018-03-06 17:00:23 +1300

    More ideas

--- neuron.h
+++ neuron.h
@@ -3,12 +3,9 @@
 
 #include <stddef.h>
 
-#define LAYERS_LEN 3 //the total number of layers (must be at least 3)
-#define LAYERS_INPUT_LEN 3 //the length of the input layer (in neurons)
-#define LAYERS_OUTPUT_LEN 1 //the length of the output layer (in neurons)
-#define LAYERS_HIDDEN_LEN 5 //the length of hidden layers (in neurons)
+#include "neuron_config.h"
 
-#define NUM_CONNECTIONS (LAYERS)
+#define NUM_CONNECTIONS (LAYERS_INPUT_LEN * LAYERS_HIDDEN_LEN + (LAYERS_HIDDEN_LEN * LAYERS_HIDDEN_LEN) * (LAYERS_LEN - 2) + LAYERS_HIDDEN_LEN * LAYERS_OUTPUT_LEN)
 
 struct NeuralNetworkConnection {
 	uint32_t Source;
@@ -19,13 +16,7 @@ struct NeuralNetworkConnection {
 struct NeuralNetwork
 {
 	//a neural network is made up of a directed acyclic graph of neurons
-	NeuralNetworkConnection Connections[LAYERS_;
-
-}
-
-struct Neuron
-{
-	uint8_t Weight;
+	NeuralNetworkConnection Connections[NUM_CONNECTIONS];
 }
 
 uint32_t
